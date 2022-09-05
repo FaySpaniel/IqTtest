98139cf543fd3bc2651d211258b968060c65e974
FaySpaniel <Fay.Spaniel@yandex.ru>
2022-09-06 00:09:53 +0300

    If

--- app/src/main/java/com/example/iqtest/Result.java
+++ app/src/main/java/com/example/iqtest/Result.java
@@ -12,6 +12,7 @@ public class Result extends AppCompatActivity {
     Button butres;
     TextView resultat;
     TextView iqPoints;
+    TextView prizvanie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +22,28 @@ public class Result extends AppCompatActivity {
         butres =(Button) findViewById(R.id.button6);
         resultat =(TextView) findViewById(R.id.textView7);
         iqPoints = (TextView) findViewById(R.id.textView6);
+        prizvanie = (TextView) findViewById(R.id.textView11);
 
         View.OnClickListener result = new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                resultat.setText("Ваш IQ Равен:");
-                iqPoints.setText(String.valueOf(MainActivity.kolvo));
+                resultat.setText("Ваш уровень интеллекта:");
+                if (MainActivity.kolvo <50 ){
+                    prizvanie.setText("ABOBOV");
+
             }
-        };
+                if (MainActivity.kolvo<100 & MainActivity.kolvo>50){
+                    prizvanie.setText("Normis");
+
+            };
+                if (MainActivity.kolvo>101){
+                    prizvanie.setText("Geniy");
+                }
+                iqPoints.setText(String.valueOf(MainActivity.kolvo));
+    };
+
+};
         butres.setOnClickListener(result);
-    }
+};
 }
